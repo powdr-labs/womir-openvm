8f5b2e464d61a271b387cdd4bd686a70163e0168
Lucas Clemente Vella <lvella@powdrlabs.com>
2025-08-20 15:57:28 +0200

    Don't write twice to same register. (#55)

--- integration/src/womir_translation.rs
+++ integration/src/womir_translation.rs
@@ -859,6 +859,8 @@ impl<'a, F: PrimeField32> Settings<'a> for OpenVMSettings<F> {
                 let input = inputs[0].start as usize;
                 let output = output.unwrap().start as usize;
 
+                let tmp = c.register_gen.allocate_type(ValType::I32).start as usize;
+
                 let shift = match op {
                     Op::I32Extend8S => 24,
                     Op::I32Extend16S => 16,
@@ -869,13 +871,15 @@ impl<'a, F: PrimeField32> Settings<'a> for OpenVMSettings<F> {
 
                 // Left shift followed by arithmetic right shift
                 vec![
-                    Directive::Instruction(ib::shl_imm(output, input, shift)),
-                    Directive::Instruction(ib::shr_s_imm(output, output, shift)),
+                    Directive::Instruction(ib::shl_imm(tmp, input, shift)),
+                    Directive::Instruction(ib::shr_s_imm(output, tmp, shift)),
                 ]
             }
             Op::I64Extend8S | Op::I64Extend16S | Op::I64Extend32S => {
                 let input = inputs[0].start as usize;
                 let output = output.unwrap().start as usize;
+
+                let tmp = c.register_gen.allocate_type(ValType::I64).start as usize;
 
                 let shift = match op {
                     Op::I64Extend8S => 56,
@@ -888,8 +892,8 @@ impl<'a, F: PrimeField32> Settings<'a> for OpenVMSettings<F> {
 
                 // Left shift followed by arithmetic right shift
                 vec![
-                    Directive::Instruction(ib::shl_imm_64(output, input, shift)),
-                    Directive::Instruction(ib::shr_s_imm_64(output, output, shift)),
+                    Directive::Instruction(ib::shl_imm_64(tmp, input, shift)),
+                    Directive::Instruction(ib::shr_s_imm_64(output, tmp, shift)),
                 ]
             }
 
