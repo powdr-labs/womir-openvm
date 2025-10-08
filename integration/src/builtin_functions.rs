fb14c9e253a244a4a2e1fb2427754be58ca4c038
Lucas Clemente Vella <lvella@powdrlabs.com>
2025-10-08 19:09:09 -0300

    Working parallel loading.

--- integration/src/builtin_functions.rs
+++ integration/src/builtin_functions.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::{HashMap, hash_map::Entry},
+    collections::HashMap,
     sync::{Mutex, atomic::AtomicU32},
 };
 
@@ -7,6 +7,7 @@ use itertools::Itertools;
 use openvm_stark_backend::p3_field::PrimeField32;
 use wasmparser::{Operator, ValType};
 use womir::loader::{
+    Module,
     blockless_dag::{BlocklessDag, Operation},
     flattening::WriteOnceAsm,
 };
@@ -71,32 +72,56 @@ const I64_CLZ_WASM: BuiltinDefinition = BuiltinDefinition {
 
 const NUM_BUILTINS: usize = 8;
 
-pub struct BuiltinFunction<F: PrimeField32> {
+pub struct BuiltinFunction {
     pub function_index: u32,
     pub definition: &'static BuiltinDefinition,
-    pub function_definition: WriteOnceAsm<Directive<F>>,
+}
+
+impl BuiltinFunction {
+    pub fn load<F: PrimeField32>(
+        self,
+        module: &Module,
+        label_gen: &AtomicU32,
+    ) -> WriteOnceAsm<Directive<F>> {
+        // Load the built-in function module.
+        let program =
+            womir::loader::load_wasm(OpenVMSettings::<F>::new(), self.definition.wasm_bytes)
+                .unwrap();
+
+        // Compile the built-in function.
+        let function = program.functions.into_iter().exactly_one().unwrap();
+        function
+            .advance_all_stages(
+                &OpenVMSettings::<F>::new(),
+                // It shouldn't make any difference which module we pass here,
+                // because the built-in function should be self-contained.
+                module,
+                self.function_index,
+                label_gen,
+                None,
+            )
+            .unwrap()
+    }
 }
 
 /// Tracks which built-in functions are used and provides functions to replace unsupported
 /// instructions with calls to these built-in functions.
-pub struct Tracker<F: PrimeField32> {
+pub struct Tracker {
     next_available_function_index: AtomicU32,
-    used_builtins: Mutex<HashMap<usize, u32>>,
-    builtin_definitions: Mutex<Vec<BuiltinFunction<F>>>,
+    used_builtins: Mutex<HashMap<usize, BuiltinFunction>>,
 }
 
-impl<F: PrimeField32> Tracker<F> {
+impl Tracker {
     pub fn new(next_available_function_index: u32) -> Self {
         Self {
             next_available_function_index: AtomicU32::new(next_available_function_index),
             used_builtins: Mutex::new(HashMap::new()),
-            builtin_definitions: Mutex::new(Vec::new()),
         }
     }
 
     /// If the given instruction is not supported natively by the VM, replace it with a call to
     /// a built-in function.
-    pub fn replace_with_builtins(&self, label_gen: &AtomicU32, dag: &mut BlocklessDag) {
+    pub fn replace_with_builtins(&self, dag: &mut BlocklessDag) {
         for node in &mut dag.nodes {
             match &mut node.operation {
                 Operation::WASMOp(op) => {
@@ -123,12 +148,12 @@ impl<F: PrimeField32> Tracker<F> {
                     };
 
                     // Get the index of the built-in function, adding it to the module if needed.
-                    let function_index = self.get_index_for_builtin(label_gen, builtin);
+                    let function_index = self.get_index_for_builtin(builtin);
                     // Replace the instruction with a call to the built-in function.
                     *op = Operator::Call { function_index };
                 }
                 Operation::Loop { sub_dag, .. } => {
-                    self.replace_with_builtins(label_gen, sub_dag);
+                    self.replace_with_builtins(sub_dag);
                 }
                 _ => (),
             }
@@ -137,63 +162,31 @@ impl<F: PrimeField32> Tracker<F> {
 
     /// Returns an iterator over the used built-in functions to be added to the final program.
     /// Sorted by function index.
-    pub fn into_used_builtins(self) -> Vec<BuiltinFunction<F>> {
+    pub fn into_used_builtins(self) -> impl Iterator<Item = BuiltinFunction> {
+        let used_builtins = self.used_builtins.into_inner().unwrap();
+
         // There can't be more used built-ins than we have defined. Otherwise some built-in
         // function would have been added more than once.
-        assert!(self.used_builtins.into_inner().unwrap().len() <= NUM_BUILTINS);
+        assert!(used_builtins.len() <= NUM_BUILTINS);
 
-        let mut result = self.builtin_definitions.into_inner().unwrap();
-        result.sort_unstable_by_key(|bf| bf.function_index);
-        result
+        used_builtins
+            .into_values()
+            .sorted_unstable_by_key(|bf| bf.function_index)
     }
 
-    fn get_index_for_builtin(
-        &self,
-        label_gen: &AtomicU32,
-        builtin: &'static BuiltinDefinition,
-    ) -> u32 {
+    fn get_index_for_builtin(&self, builtin: &'static BuiltinDefinition) -> u32 {
         let key = builtin as *const _ as usize;
 
-        let function_index = match self.used_builtins.lock().unwrap().entry(key) {
-            Entry::Occupied(entry) => {
-                // This built-in function was already loaded, we can return early.
-                return *entry.into_mut();
-            }
-            Entry::Vacant(entry) => *entry.insert(
-                self.next_available_function_index
-                    .fetch_add(1, std::sync::atomic::Ordering::Relaxed),
-            ),
-        };
-
-        // Load the built-in function module.
-        let program =
-            womir::loader::load_wasm(OpenVMSettings::<F>::new(), builtin.wasm_bytes).unwrap();
-
-        // Compile the built-in function.
-        let function = program.functions.into_iter().exactly_one().unwrap();
-        let mut function_definition = function
-            .advance_all_stages(
-                &OpenVMSettings::<F>::new(),
-                // It shouldn't make any difference which module we pass here,
-                // because the built-in function should be self-contained.
-                &program.m,
-                0,
-                label_gen,
-                None,
-            )
-            .unwrap();
-        // Fix-up the function index for the module it will be added to.
-        function_definition.func_idx = function_index;
-
-        self.builtin_definitions
+        self.used_builtins
             .lock()
             .unwrap()
-            .push(BuiltinFunction {
-                function_index,
+            .entry(key)
+            .or_insert_with(|| BuiltinFunction {
                 definition: builtin,
-                function_definition,
-            });
-
-        function_index
+                function_index: self
+                    .next_available_function_index
+                    .fetch_add(1, std::sync::atomic::Ordering::Relaxed),
+            })
+            .function_index
     }
 }
