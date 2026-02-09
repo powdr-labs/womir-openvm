6241cbe184f7a14106d65ea3d36460ce53a9c5b9
Georg Wiese <georgwiese@gmail.com>
2026-02-09 20:18:25 +0100

    Remove CUDA

--- extensions/womir_circuit/src/base_alu/mod.rs
+++ extensions/womir_circuit/src/base_alu/mod.rs
@@ -9,11 +9,6 @@ mod core;
 mod execution;
 pub use core::*;
 
-#[cfg(feature = "cuda")]
-mod cuda;
-#[cfg(feature = "cuda")]
-pub use cuda::*;
-
 pub type Rv32BaseAluAir =
     VmAirWrapper<Rv32BaseAluAdapterAir, BaseAluCoreAir<RV32_REGISTER_NUM_LIMBS, RV32_CELL_BITS>>;
 pub type Rv32BaseAluExecutor = BaseAluExecutor<
