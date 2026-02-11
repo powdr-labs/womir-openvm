d808a4170744dc3ff02eb7efd3980f6d7dfcd446
Georg Wiese <georgwiese@gmail.com>
2026-02-11 14:37:20 +0100

    Fix memory config

--- extensions/womir_circuit/src/memory_config.rs
+++ extensions/womir_circuit/src/memory_config.rs
@@ -1,17 +1,27 @@
-use openvm_circuit::{arch::MemoryConfig, system::memory::online::GuestMemory};
+use openvm_circuit::{
+    arch::{ADDR_SPACE_OFFSET, DEFAULT_MAX_NUM_PUBLIC_VALUES, MemoryConfig},
+    system::memory::{
+        POINTER_MAX_BITS, merkle::public_values::PUBLIC_VALUES_AS, online::GuestMemory,
+    },
+};
+use openvm_instructions::riscv::{RV32_MEMORY_AS, RV32_REGISTER_AS};
 
 /// This address space is only used for execution, to store the frame pointer at address 0.
 pub const FP_AS: u32 = 5;
 
-/// Same as `MemoryConfig::default()`, but with FP address space.
+/// Same as `MemoryConfig::default()`, but:
+/// - Register address space has the same size as the memory address space.
+/// - Removed native address space (only needed in recursion).
+/// - Added address space for FP. It has one cell to store the current FP value.
 pub fn memory_config_with_fp() -> MemoryConfig {
-    let mut memory_config = MemoryConfig::default();
-    assert_eq!(
-        memory_config.addr_spaces[FP_AS as usize].num_cells, 0,
-        "FP address space must be unused in default config"
-    );
-    memory_config.addr_spaces[FP_AS as usize].num_cells = 1;
-    memory_config
+    let mut addr_spaces =
+        MemoryConfig::empty_address_space_configs((1 << 3) + ADDR_SPACE_OFFSET as usize);
+    const MAX_CELLS: usize = 1 << 29;
+    addr_spaces[RV32_REGISTER_AS as usize].num_cells = MAX_CELLS;
+    addr_spaces[RV32_MEMORY_AS as usize].num_cells = MAX_CELLS;
+    addr_spaces[PUBLIC_VALUES_AS as usize].num_cells = DEFAULT_MAX_NUM_PUBLIC_VALUES;
+    addr_spaces[FP_AS as usize].num_cells = size_of::<u32>();
+    MemoryConfig::new(3, addr_spaces, POINTER_MAX_BITS, 29, 17, 32)
 }
 
 /// Utility trait to read the frame pointer from memory, used in preflight and execution.
