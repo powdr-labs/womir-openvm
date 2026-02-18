0d65691297ae13509bd1362631c0bf2e353288c0
Thibaut Schaeffer <thibaut@powdrlabs.com>
2026-02-18 18:06:55 +0100

    clean: remove limb bit generics (#189)
    * remove limb bit generics
    
    * fmt
    
    * fix
    
    * more removals
    
    * update machine

--- extensions/womir_circuit/src/less_than/mod.rs
+++ extensions/womir_circuit/src/less_than/mod.rs
@@ -14,15 +14,10 @@ pub use execution::LessThanExecutor;
 // 32-bit type aliases
 pub type Rv32LessThanAir =
     VmAirWrapper<Rv32BaseAluAdapterAir, LessThanCoreAir<RV32_REGISTER_NUM_LIMBS, RV32_CELL_BITS>>;
-pub type Rv32LessThanExecutor =
-    LessThanExecutor<RV32_REGISTER_NUM_LIMBS, W32_REG_OPS, W32_REG_OPS, RV32_CELL_BITS>;
+pub type Rv32LessThanExecutor = LessThanExecutor<RV32_REGISTER_NUM_LIMBS, W32_REG_OPS, W32_REG_OPS>;
 pub type Rv32LessThanChip<F> = VmChipWrapper<
     F,
-    LessThanFiller<
-        Rv32BaseAluAdapterFiller<RV32_CELL_BITS>,
-        RV32_REGISTER_NUM_LIMBS,
-        RV32_CELL_BITS,
-    >,
+    LessThanFiller<Rv32BaseAluAdapterFiller, RV32_REGISTER_NUM_LIMBS, RV32_CELL_BITS>,
 >;
 
 // 64-bit type aliases
@@ -32,12 +27,11 @@ pub type LessThan64Air = VmAirWrapper<
     BaseAluAdapterAirDifferentInputsOutputs<W64_NUM_LIMBS, W64_REG_OPS, W32_REG_OPS>,
     LessThanCoreAir<W64_NUM_LIMBS, RV32_CELL_BITS>,
 >;
-pub type LessThan64Executor =
-    LessThanExecutor<W64_NUM_LIMBS, W64_REG_OPS, W32_REG_OPS, RV32_CELL_BITS>;
+pub type LessThan64Executor = LessThanExecutor<W64_NUM_LIMBS, W64_REG_OPS, W32_REG_OPS>;
 pub type LessThan64Chip<F> = VmChipWrapper<
     F,
     LessThanFiller<
-        BaseAluAdapterFillerDifferentInputsOutputs<W64_REG_OPS, W32_REG_OPS, RV32_CELL_BITS>,
+        BaseAluAdapterFillerDifferentInputsOutputs<W64_REG_OPS, W32_REG_OPS>,
         W64_NUM_LIMBS,
         RV32_CELL_BITS,
     >,
